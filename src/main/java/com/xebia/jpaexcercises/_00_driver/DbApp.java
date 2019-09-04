e56aad11f5d58c01f89e1e717303a1cb6eaad602
Sugandha Sapra <Sugandha.Sapra@xebia.com>
2019-09-04 16:49:42 +0530

    updated

--- src/main/java/com/xebia/jpaexcercises/_00_driver/DbApp.java
+++ src/main/java/com/xebia/jpaexcercises/_00_driver/DbApp.java
@@ -1,10 +1,19 @@
 package com.xebia.jpaexcercises._00_driver;
 
-import com.xebia.jpaexcercises._01_access.EmployeeWithFieldAccess;
+//import com.xebia.jpaexcercises._01_access.EmployeeWithFieldAccess;
+//import com.xebia.jpaexcercises._01_access.EmployeeWithMixedAccess;
+//import com.xebia.jpaexcercises._01_access.EmployeeWithPropertyAccess;
+//import com.xebia.jpaexcercises._03_lazy.Employee;
+//import com.xebia.jpaexcercises._04_lob.Employee;
+import com.xebia.jpaexcercises._05_enum.Employee;
+import com.xebia.jpaexcercises._05_enum.EmployeeType;
+
 
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
+import java.io.File;
+import java.io.FileInputStream;
 
 public class DbApp {
 
@@ -13,15 +22,70 @@ public class DbApp {
         EntityManager entityManager = entityManagerFactory.createEntityManager();
         entityManager.getTransaction().begin();
 
-        EmployeeWithFieldAccess employee = new EmployeeWithFieldAccess();
+        /*EmployeeWithFieldAccess employee = new EmployeeWithFieldAccess();
         employee.setId(1L);
         employee.setName("Test Employee");
         employee.setEmail("test@test.com");
         entityManager.persist(employee);
-        entityManager.getTransaction().commit();
+        EmployeeWithFieldAccess foundEmployeeWithFieldAccess = entityManager.find(EmployeeWithFieldAccess.class, 1L);
+        System.out.println(foundEmployeeWithFieldAccess);
 
-        EmployeeWithFieldAccess foundEmployee = entityManager.find(EmployeeWithFieldAccess.class, 1L);
-        System.out.println(foundEmployee);
+        EmployeeWithMixedAccess employeeWithMixedAccess = new EmployeeWithMixedAccess();
+        employeeWithMixedAccess.setId(2L);
+        employeeWithMixedAccess.setUsername("Test Employee");
+        employeeWithMixedAccess.setEmailAddress("test@test.com");
+        entityManager.persist(employeeWithMixedAccess);
+        EmployeeWithMixedAccess foundEmployeeWithMixedAccess=entityManager.find(EmployeeWithMixedAccess.class,2L);
+        System.out.println(foundEmployeeWithMixedAccess);
+
+        EmployeeWithPropertyAccess employeeWithPropertyAccess = new EmployeeWithPropertyAccess();
+        employeeWithPropertyAccess.setId(3L);
+        employeeWithPropertyAccess.setName("Test Employee");
+        employeeWithPropertyAccess.setEmail("test@test.com");
+        entityManager.persist(employeeWithPropertyAccess);
+        //entityManager.getTransaction().commit();
+        EmployeeWithPropertyAccess foundEmployeeWithPropertyAccess=entityManager.find(EmployeeWithPropertyAccess.class,3L);
+        System.out.println(foundEmployeeWithPropertyAccess);*/
+
+
+      /*  Employee lazyEmployee= new Employee();
+        lazyEmployee.setId(1L);
+        lazyEmployee.setName("Sample");
+        lazyEmployee.setBio("Sample");
+        entityManager.persist(lazyEmployee);
+        entityManager.getTransaction().commit();
+        entityManager.close();
+        //entityManager = entityManagerFactory.createEntityManager();
+        Employee foundEmployee=entityManager.find(Employee.class,1L);
+        System.out.println(foundEmployee);*/
+
+
+       /* Employee lobEmployee=new Employee();
+        lobEmployee.setId(1L);
+        lobEmployee.setName("Sample");
+        lobEmployee.setBio("Sample");
+        File file = new File("D:\\personal\\pic.jpg");
+        byte[] bFile = new byte[(int) file.length()];
+        try {
+            FileInputStream fileInputStream = new FileInputStream(file);
+            //convert file into array of bytes
+            fileInputStream.read(bFile);
+            fileInputStream.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        lobEmployee.setPicture(bFile);
+
+        entityManager.persist(lobEmployee);*/
+
+
+       Employee  enumEmployee=new Employee();
+        enumEmployee.setBio("Sample");
+        enumEmployee.setEmployeeType(EmployeeType.FULL_TIME);
+        enumEmployee.setId(1L);
+        enumEmployee.setName("Sample");
+        entityManager.persist(enumEmployee);
+        entityManager.getTransaction().commit();
         entityManager.close();
         entityManagerFactory.close();
 
--- src/main/java/com/xebia/jpaexcercises/_01_access/EmployeeWithFieldAccess.java
+++ src/main/java/com/xebia/jpaexcercises/_01_access/EmployeeWithFieldAccess.java
@@ -11,25 +11,27 @@ public class EmployeeWithFieldAccess {
     private String name;
     private String email;
 
-    public long getId() {
+  /*  public long getId() {
         return id;
-    }
+    }*/
 
     public void setId(long id) {
         this.id = id;
     }
 
+/*
     public String getName() {
         return name;
     }
+*/
 
     public void setName(String name) {
         this.name = name;
     }
 
-    public String getEmail() {
+  /*  public String getEmail() {
         return email;
-    }
+    }*/
 
     public void setEmail(String email) {
         this.email = email;
